f700f7620b18156439f23e23e0f9a919b897d4b5
Unknown <hamedsafaei97@yahoo.com>
2019-01-17 15:49:36 +0330

    add tanks agents

---
+++ UnitySDK/Assets/ML-Agents/Examples/Tanks/Scripts/RL/Tank1Agent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class Tank1Agent : Agent {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
---
+++ UnitySDK/Assets/ML-Agents/Examples/Tanks/Scripts/RL/Tank2Agent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class Tank2Agent : Agent {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
